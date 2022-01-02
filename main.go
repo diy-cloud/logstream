89db24f459138d83a6700c7ba5de93833ebf6400
snowmerak <snowmerak@outlook.com>
2022-01-02 16:00:25 +0900

    change error to generic

--- main.go
+++ main.go
@@ -1,38 +1,38 @@
-package errorstream
+package msgbuf
 
 import (
 	"github.com/Workiva/go-datastructures/trie/ctrie"
-	"github.com/snowmerak/error-stream/unlock"
+	"github.com/snowmerak/msgbuf/unlock"
 )
 
-type ErrorStream struct {
+type MessageBuffer[T any] struct {
 	trie       *ctrie.Ctrie
 	bufferSize int
 }
 
-func New(bufferSize int) ErrorStream {
-	return ErrorStream{
+func New[T any](bufferSize int) MessageBuffer[T] {
+	return MessageBuffer[T]{
 		trie:       ctrie.New(nil),
 		bufferSize: bufferSize,
 	}
 }
 
-func (e *ErrorStream) EnQueue(topic string, err error) {
+func (e *MessageBuffer[T]) EnQueue(topic string, value T) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
-		e.trie.Insert(key, unlock.NewRingBuffer(e.bufferSize))
+		e.trie.Insert(key, unlock.NewRingBuffer[T](e.bufferSize))
 	}
 	p, _ := e.trie.Lookup(key)
-	ringBuffer := p.(*unlock.RingBuffer)
-	ringBuffer.EnQueue(err)
+	ringBuffer := p.(*unlock.RingBuffer[T])
+	ringBuffer.EnQueue(value)
 }
 
-func (e *ErrorStream) DeQueue(topic string) (error, bool) {
+func (e *MessageBuffer[T]) DeQueue(topic string) (value T, exists bool) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
-		return nil, false
+		return value, false
 	}
 	p, _ := e.trie.Lookup(key)
-	ringBuffer := p.(*unlock.RingBuffer)
+	ringBuffer := p.(*unlock.RingBuffer[T])
 	return ringBuffer.DeQueue(), true
 }
