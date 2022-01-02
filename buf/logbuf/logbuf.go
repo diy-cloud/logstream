1785b1836ac48edf5a5159683414d5342954356e
snowmerak <snowmerak@outlook.com>
2022-01-03 01:36:55 +0900

    change type

--- buf/logbuf/logbuf.go
+++ buf/logbuf/logbuf.go
@@ -1,38 +1,39 @@
-package strbuf
+package logbuf
 
 import (
 	"github.com/Workiva/go-datastructures/trie/ctrie"
+	"github.com/snowmerak/msgbuf/log"
 	"github.com/snowmerak/msgbuf/unlock"
 )
 
-type StringBuffer struct {
+type LogBuffer struct {
 	trie       *ctrie.Ctrie
 	bufferSize int
 }
 
-func New(bufferSize int) StringBuffer {
-	return StringBuffer{
+func New(bufferSize int) LogBuffer {
+	return LogBuffer{
 		trie:       ctrie.New(nil),
 		bufferSize: bufferSize,
 	}
 }
 
-func (e *StringBuffer) EnQueue(topic string, value string) {
+func (e *LogBuffer) EnQueue(topic string, value log.Log) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
-		e.trie.Insert(key, unlock.NewStringRingBuffer(e.bufferSize))
+		e.trie.Insert(key, unlock.NewLogRingBuffer(e.bufferSize))
 	}
 	p, _ := e.trie.Lookup(key)
-	ringBuffer := p.(*unlock.StringRingBuffer)
+	ringBuffer := p.(*unlock.LogRingBuffer)
 	ringBuffer.EnQueue(value)
 }
 
-func (e *StringBuffer) DeQueue(topic string) (value string, exists bool) {
+func (e *LogBuffer) DeQueue(topic string) (value log.Log, exists bool) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
 		return value, false
 	}
 	p, _ := e.trie.Lookup(key)
-	ringBuffer := p.(*unlock.StringRingBuffer)
+	ringBuffer := p.(*unlock.LogRingBuffer)
 	return ringBuffer.DeQueue(), true
 }
