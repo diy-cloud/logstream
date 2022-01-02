de9e13349b5d5e8e4b647f02169a004a91b9ba17
snowmerak <snowmerak@outlook.com>
2022-01-03 02:24:00 +0900

    Update stdout.go

--- log/recordable/stdout.go
+++ log/recordable/stdout.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/snowmerak/msgbuf/log"
 	"github.com/snowmerak/msgbuf/log/loglevel"
@@ -11,14 +12,16 @@ import (
 
 type Stdout struct {
 	sync.Mutex
-	level  loglevel.LogLevel
-	writer *bufio.Writer
+	level       loglevel.LogLevel
+	writer      *bufio.Writer
+	displayTime bool
 }
 
-func NewStdout(level loglevel.LogLevel) log.Writable {
+func NewStdout(level loglevel.LogLevel, displayTime bool) log.Writable {
 	return &Stdout{
-		writer: bufio.NewWriter(os.Stdout),
-		level:  level,
+		writer:      bufio.NewWriter(os.Stdout),
+		level:       level,
+		displayTime: displayTime,
 	}
 }
 
@@ -26,6 +29,10 @@ func (s *Stdout) Write(level loglevel.LogLevel, value []byte) error {
 	s.Lock()
 	defer s.Unlock()
 	if loglevel.Available(s.level, level) {
+		if s.displayTime {
+			s.writer.Write([]byte(time.Now().Format(time.RFC3339)))
+			s.writer.Write([]byte(" "))
+		}
 		s.writer.Write(value)
 		s.writer.WriteByte('\n')
 		return s.writer.Flush()
