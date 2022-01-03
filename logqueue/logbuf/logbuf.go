5ab747100cc4b4b7c925ddb9515aaf1fed08e08b
snowmerak <snowmerak@outlook.com>
2022-01-03 13:42:00 +0900

    move deps

--- logqueue/logbuf/logbuf.go
+++ logqueue/logbuf/logbuf.go
@@ -1,27 +1,34 @@
 package logbuf
 
 import (
+	"errors"
+
 	"github.com/Workiva/go-datastructures/trie/ctrie"
 	"github.com/snowmerak/logstream/log"
-	"github.com/snowmerak/logstream/unlock"
+	"github.com/snowmerak/logstream/logqueue"
 )
 
 type LogBuffer struct {
 	trie       *ctrie.Ctrie
 	bufferSize int
+	signals    map[string]chan struct{}
 }
 
 func New(bufferSize int) *LogBuffer {
 	return &LogBuffer{
 		trie:       ctrie.New(nil),
 		bufferSize: bufferSize,
+		signals:    map[string]chan struct{}{},
 	}
 }
 
-func (e *LogBuffer) AddTopic(topic string) {
+func (e *LogBuffer) AddTopic(topic string, signal chan struct{}) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
-		e.trie.Insert(key, unlock.NewLogRingBuffer(e.bufferSize))
+		e.trie.Insert(key, logqueue.New(e.bufferSize))
+	}
+	if _, ok := e.signals[topic]; !ok {
+		e.signals[topic] = signal
 	}
 }
 
@@ -30,24 +37,28 @@ func (e *LogBuffer) RemoveTopic(topic string) {
 	if _, ok := e.trie.Lookup(key); ok {
 		e.trie.Remove(key)
 	}
+	delete(e.signals, topic)
 }
 
 func (e *LogBuffer) EnQueue(topic string, value log.Log) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
-		e.trie.Insert(key, unlock.NewLogRingBuffer(e.bufferSize))
+		e.trie.Insert(key, logqueue.New(e.bufferSize))
 	}
 	p, _ := e.trie.Lookup(key)
-	ringBuffer := p.(*unlock.LogRingBuffer)
-	ringBuffer.EnQueue(value)
+	ringBuffer := p.(*logqueue.LogQueue)
+	ringBuffer.Push(value)
+	if e.signals[topic] != nil {
+		e.signals[topic] <- struct{}{}
+	}
 }
 
-func (e *LogBuffer) DeQueue(topic string) (value log.Log, exists bool) {
+func (e *LogBuffer) DeQueue(topic string) (log.Log, error) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
-		return value, false
+		return log.Log{}, errors.New("LogBuffer.DeQueue: topic not found")
 	}
 	p, _ := e.trie.Lookup(key)
-	ringBuffer := p.(*unlock.LogRingBuffer)
-	return ringBuffer.DeQueue(), true
+	ringBuffer := p.(*logqueue.LogQueue)
+	return ringBuffer.Pop()
 }
