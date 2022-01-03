9cd5cc259977fdfb2e8b81d1c220c30060042b01
snowmerak <snowmerak@outlook.com>
2022-01-03 13:42:10 +0900

    Create pq.go

---
+++ logqueue/pq.go
@@ -0,0 +1,43 @@
+package logqueue
+
+import (
+	"errors"
+
+	"github.com/Workiva/go-datastructures/queue"
+	"github.com/snowmerak/logstream/log"
+)
+
+type LogQueue struct {
+	queue *queue.PriorityQueue
+}
+
+func New(size int) *LogQueue {
+	return &LogQueue{
+		queue: queue.NewPriorityQueue(size, false),
+	}
+}
+
+func (lq *LogQueue) Push(log log.Log) error {
+	if lq == nil {
+		return errors.New("LogQueue.Push: LogQueue is nil")
+	}
+	return lq.queue.Put(log)
+}
+
+func (lq *LogQueue) Pop() (log.Log, error) {
+	if lq == nil {
+		return log.Log{}, errors.New("LogQueue.Pop: LogQueue is nil")
+	}
+	item, err := lq.queue.Get(1)
+	if err != nil {
+		return log.Log{}, err
+	}
+	return item[0].(log.Log), nil
+}
+
+func (lq *LogQueue) Size() int {
+	if lq == nil {
+		return 0
+	}
+	return lq.queue.Len()
+}
