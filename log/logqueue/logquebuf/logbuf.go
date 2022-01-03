69811da51cea0dae6b5b8d1ed1917371a1ca0b5e
snowmerak <snowmerak@outlook.com>
2022-01-03 17:27:46 +0900

    Rename Buffer to QueueBuffer

--- log/logqueue/logquebuf/logbuf.go
+++ log/logqueue/logquebuf/logbuf.go
@@ -8,21 +8,21 @@ import (
 	"github.com/snowmerak/logstream/log/logqueue"
 )
 
-type LogBuffer struct {
+type LogQueueBuffer struct {
 	trie       *ctrie.Ctrie
 	bufferSize int
 	signals    map[string]chan struct{}
 }
 
-func New(bufferSize int) *LogBuffer {
-	return &LogBuffer{
+func New(bufferSize int) *LogQueueBuffer {
+	return &LogQueueBuffer{
 		trie:       ctrie.New(nil),
 		bufferSize: bufferSize,
 		signals:    map[string]chan struct{}{},
 	}
 }
 
-func (e *LogBuffer) AddTopic(topic string, signal chan struct{}) {
+func (e *LogQueueBuffer) AddTopic(topic string, signal chan struct{}) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
 		e.trie.Insert(key, logqueue.New(e.bufferSize))
@@ -32,7 +32,7 @@ func (e *LogBuffer) AddTopic(topic string, signal chan struct{}) {
 	}
 }
 
-func (e *LogBuffer) RemoveTopic(topic string) {
+func (e *LogQueueBuffer) RemoveTopic(topic string) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); ok {
 		e.trie.Remove(key)
@@ -40,25 +40,25 @@ func (e *LogBuffer) RemoveTopic(topic string) {
 	delete(e.signals, topic)
 }
 
-func (e *LogBuffer) EnQueue(topic string, value log.Log) {
+func (e *LogQueueBuffer) EnQueue(topic string, value log.Log) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
 		e.trie.Insert(key, logqueue.New(e.bufferSize))
 	}
 	p, _ := e.trie.Lookup(key)
-	ringBuffer := p.(*logqueue.LogQueue)
+	ringBuffer := p.(log.DataStructure)
 	ringBuffer.Push(value)
 	if e.signals[topic] != nil {
 		e.signals[topic] <- struct{}{}
 	}
 }
 
-func (e *LogBuffer) DeQueue(topic string) (log.Log, error) {
+func (e *LogQueueBuffer) DeQueue(topic string) (log.Log, error) {
 	key := []byte(topic)
 	if _, ok := e.trie.Lookup(key); !ok {
 		return log.Log{}, errors.New("LogBuffer.DeQueue: topic not found")
 	}
 	p, _ := e.trie.Lookup(key)
-	ringBuffer := p.(*logqueue.LogQueue)
+	ringBuffer := p.(log.DataStructure)
 	return ringBuffer.Pop()
 }
